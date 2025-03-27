41686a37febe37fdf16a125550cf120163cd34ab
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-03-26 19:01:27 -0600

    added count words funtion

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const textArea = document.getElementById("text-input")
-const characterCount = document.getElementById("char-count");
+const characterCounter = document.getElementById("char-count");
+const wordCounter = document.getElementById("word-count");
+
 
 function CountCharacters() {
-  characterCount.textContent = textArea.value.length;
+  characterCounter.textContent = textArea.value.length;
 // if exculeSpaces
 }
 
 textArea.addEventListener("keyup", CountCharacters)
+
+
+function CountWords() {
+  let text = textArea.value;
+  let trimmed = text.trim();
+
+  if (trimmed === "") return 0;
+
+  let words = trimmed.split(/\s+/);
+
+  wordCounter.textContent = words.length
+}
+
+textArea.addEventListener("keyup", CountWords)
+
 
