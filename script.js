3774ccbe7bb7bfcc33c38a5798b992506bd45036
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-03-26 19:10:20 -0600

    added count sentences  funtion

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textArea = document.getElementById("text-input")
 const characterCounter = document.getElementById("char-count");
 const wordCounter = document.getElementById("word-count");
+const sentenceCounter = document.getElementById("sentence-count")
 
 
 function CountCharacters() {
@@ -25,3 +26,15 @@ function CountWords() {
 textArea.addEventListener("keyup", CountWords)
 
 
+function CountSentences() {
+  let text = textArea.value;
+  let trimmed = text.trim();
+
+  if (trimmed === "") return 0;
+
+  let sentences = trimmed.split(/[.!?]+/).filter(sentence => sentence.trim().length > 0);
+
+  sentenceCounter.textContent = sentences.length
+}
+
+textArea.addEventListener("keyup", CountSentences)
